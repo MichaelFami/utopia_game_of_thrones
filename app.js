a751104cd25c59f5ab70669714fc06f6ca0df3a2
MichaelFami <michaelfami@gmail.com>
2023-09-26 10:47:30 -0500

    new elements get created

--- app.js
+++ app.js
@@ -26,23 +26,316 @@ const currentArchers = document.querySelector("#currentArchers");
 const currentTrebuchets = document.querySelector("#currentTrebuchets");
 const currentDragons = document.querySelector("#currentDragons");
 
-const INIT_STATE = {
+const players = [
+  {
+  name: "Player1",
+  House: "",
   gold: 200,
   food: 50,
   land: 20,
   houses: 5,
   farms: 5,
   banks: 5,
-  army:{
+  army: {
     generals: 2,
-  soldiers: 300,
-  cavalry: 100,
-  archers: 100,
-  trebuchets: 10,
-  dragons: 1,
+    soldiers: 300,
+    cavalry: 100,
+    archers: 100,
+    trebuchets: 10,
+    dragons: 0,
+  },
+}, 
+];
 
+let dropDownList = document.querySelector("#numberOfPlayers");
+dropDownList.onchange = (e) => {
+  let selectedI = dropDownList.selectedIndex;
+  let selectedOption = dropDownList.options[selectedI];
+  let selectedNumberOfPlayers = parseInt(selectedOption.value);
+
+  let htmlToAdd=""
+
+
+  for (let i = 1; i < selectedNumberOfPlayers; i++) {
+    players[i] = {
+        name: "Player"+[i+1],
+        House: "",
+        gold: 200,
+        food: 50,
+        land: 20,
+        houses: 5,
+        farms: 5,
+        banks: 5,
+        army: {
+          generals: 2,
+          soldiers: 300,
+          cavalry: 100,
+          archers: 100,
+          trebuchets: 10,
+          dragons: 0,
+        },
+      }
+      
+htmlToAdd+=`
+<div id="player${[i]}Screen">
+<h3>Player ${[i]}</h3>
+<table class="buildingsTable">
+    <tr><th>Buildings</th></tr>
+    <tr>
+        <th>
+
+        </th>
+        <th>Cost to Build</th>
+        <th>Own</th>
+        
+        <th>Under Construction</th>
+        <th>Build</th>
+    </tr>
+    <tr>
+        <th>Houses</th>
+        <td>5 gold and 1 land</td>
+        <td id="housesBuilt${[i]}">
+            
+        </td>
+        <td id="housesUnderConstruction${[i]}">
+            
+        </td>
+        <td>
+            <input id="buildHouseInput${[i]}"></input> 
+        </td>
+        
+    </tr>
+    <tr>
+        <th>Farms</th>
+        <td>5 gold and 1 land</td>
+
+        <td id="farmsBuilt${[i]}">
+            
+        </td>
+        <td id="farmsUnderConstruction${[i]}">
+            
+        </td>
+        <td>
+            <input id="buildFarmInput${[i]}"></input> 
+        </td>
+    </tr>
+    <tr>
+        <th>Banks</th>
+        <td>10 gold and 1 land</td>
+
+        <td id="banksBuilt${[i]}">
+            
+        </td>
+        <td id="banksUnderConstruction${[i]}">
+            
+        </td>
+        <td>
+            <input id="buildBankInput${[i]}"></input> 
+        </td>
+    </tr>
+    <tr>
+        <th>Barracks</th>
+        <td>
+            
+        </td>
+    </tr>
+    <tr>
+        <th>Castles</th>
+        <td>
+            
+        </td>
+    </tr>
+    <tr>
+        <th>Armories</th>
+        <td>
+            
+        </td>
+    </tr>
+</table>
+<button id="buildButton">Build</button>
+<table id="resourcesTable">
+    <tr><th>Resources</th></tr>
+    <tr>
+        <th>
+
+        </th>
+        <th>Own</th>
+        <th>Per Turn</th>
+       
+    </tr>
+    <tr>
+        <th>Gold</th>
+        <td id="currentGold">
+            
+        </td>
+        <td id="goldPerTurn"></td>
+    </tr>
+    <tr>
+        <th>Food</th>
+        <td id="currentFood">
+            
+        </td>
+        <td id="foodPerTurn"></td>
+    </tr>
+    <tr>
+        <th>Land</th>
+        <td id="currentLand">
+            
+        </td>
+    </tr>
+    
+</table>
+<table id="militaryTable">
+    <tr><th>Military</th></tr>
+    <tr>
+        <th>
+
+        </th>
+        <th>Own</th>
+        <th>Currently Training</th>
+        <th>Recruit</th>
+    </tr>
+    <tr>
+        <th>Generals</th>
+        <td id="currentGenerals">
+            
+        </td>
+        <td></td>
+        <td>
+            <input id="recruitGeneralInput"></input> 
+        </td>
+    </tr>
+    <tr>
+        <th>Soldiers</th>
+        <td id="currentSoldiers">
+            
+        </td>
+        <td></td>
+        <td>
+            <input id="recruitSoldierInput"></input> 
+        </td>
+
+    </tr>
+    <tr>
+        <th>Cavalry</th>
+        <td id="currentCavalry">
+            
+        </td>
+        <td></td>
+        <td>
+            <input id="recruitCavalryInput"></input> 
+        </td>
+    </tr>
+    <tr>
+        <th>Archers</th>
+        <td id="currentArchers">
+            
+        </td>
+        <td></td>
+        <td>
+            <input id="recruitArcherInput"></input> 
+        </td>
+    </tr>
+    <tr>
+        <th>Trebuchets</th>
+        <td id="currentTrebuchets">
+            
+        </td>
+        <td></td>
+        <td>
+            <input id="recruitTrebuchetInput"></input> 
+        </td>
+    </tr>
+    <tr>
+        <th>Dragons</th>
+        <td id="currentDragons">
+            
+        </td>
+        <td></td>
+        <td>
+            <input id="recruitDragonInput"></input> 
+        </td>
+    </tr>
+    
+</table>
+<button id="recruitButton">Recruit</button>
+<table id="opponentsTable">
+    <tr><th>Opponents</th></tr>
+    <tr>
+        <th>
+
+        </th>
+        <th>Military Size</th>
+        <th>Amount of Land</th>
+        <th>Amount of Gold</th>
+        <th>Battle</th>
+    </tr>
+    <tr>
+        <th>Stark</th>
+        <td id="starkArmy">
+            
+        </td>
+        <td>
+            
+        </td>
+        <td>
+            
+        </td>
+        <td>
+            <button class="battleButton" id="stark">Battle!</button> 
+         </td>
+    </tr>
+    <tr>
+        <th>Lannister</th>
+        <td>
+            
+        </td>
+        <td>
+            
+        </td>
+        <td>
+            
+        </td>
+        <td>
+            <button class="battleButton" id="lannister">Battle!</button> 
+         </td>
+    </tr>
+    <tr>
+        <th>Targaryen</th>
+        <td>
+            
+        </td>
+        <td>
+            
+        </td>
+        <td>
+            
+        </td>
+        <td>
+           <button class="battleButton" id="targaryen">Battle!</button> 
+        </td>
+    </tr>
+   
+</table>
+
+
+</div>`
+
+
+
+      
   }
-  
+
+  let screenContainer = document.querySelector("#screenContainer")
+screenContainer.innerHTML=htmlToAdd
+
+
+
+
+
+
+
+  console.log(players);
 };
 
 const starkResources = {
@@ -86,13 +379,11 @@ const renderStats = () => {
   currentArchers.innerHTML = state.army.archers;
   currentTrebuchets.innerHTML = state.army.trebuchets;
   currentDragons.innerHTML = state.army.dragons;
-
-
 };
 
 const init = () => {
   state = {
-    ...INIT_STATE,
+    ...players[0],
   };
 
   cycles = 0;
@@ -177,42 +468,48 @@ const handleBtnClick = () => {
   render();
 };
 
-const battleCalculator=(chosenOpponentArmy)=>{
-    let myMilitaryPower = (
-        (state.army.generals*50)+(state.army.soldiers*1)+(state.army.cavalry*3)+(state.army.archers*2)+(state.army.trebuchets*100)+(state.army.dragons*500)
-    )
-    let opponentMilitaryPower=(
-        (chosenOpponentArmy.generals*50)+(chosenOpponentArmy.soldiers*1)+(chosenOpponentArmy.cavalry*3)+(chosenOpponentArmy.archers*2)+(chosenOpponentArmy.trebuchets*100)+(chosenOpponentArmy.dragons*500)
-    )
-    if (myMilitaryPower>opponentMilitaryPower) {
-        Object.keys(chosenOpponentArmy).forEach(element => {
-            chosenOpponentArmy[element] =Math.floor(chosenOpponentArmy[element]*0.5)
-            console.log(chosenOpponentArmy)
-
-        });
-    } else if(opponentMilitaryPower>myMilitaryPower){
-        state.army.forEach(element => {
-            state.army[element] =Math.floor(state.army[element]*0.5)
-        });
-    }else{
-        return
-    }
-   
-}
+const battleCalculator = (chosenOpponentArmy) => {
+  let myMilitaryPower =
+    state.army.generals * 50 +
+    state.army.soldiers * 1 +
+    state.army.cavalry * 3 +
+    state.army.archers * 2 +
+    state.army.trebuchets * 100 +
+    state.army.dragons * 500;
+  let opponentMilitaryPower =
+    chosenOpponentArmy.generals * 50 +
+    chosenOpponentArmy.soldiers * 1 +
+    chosenOpponentArmy.cavalry * 3 +
+    chosenOpponentArmy.archers * 2 +
+    chosenOpponentArmy.trebuchets * 100 +
+    chosenOpponentArmy.dragons * 500;
+  if (myMilitaryPower > opponentMilitaryPower) {
+    Object.keys(chosenOpponentArmy).forEach((element) => {
+      chosenOpponentArmy[element] = Math.floor(
+        chosenOpponentArmy[element] * 0.5
+      );
+      console.log(chosenOpponentArmy);
+    });
+  } else if (opponentMilitaryPower > myMilitaryPower) {
+    state.army.forEach((element) => {
+      state.army[element] = Math.floor(state.army[element] * 0.5);
+    });
+  } else {
+    return;
+  }
+};
 
 battleButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    let chosenOpponentArmy = opponents[button.id].army
-    battleCalculator(chosenOpponentArmy)
-    render()
+    let chosenOpponentArmy = opponents[button.id].army;
+    battleCalculator(chosenOpponentArmy);
+    render();
   });
-  
 });
 
 buildButton.addEventListener("click", handleBtnClick);
 
 init();
-
 
 // formulas for tracking current resources and spending resources
 
