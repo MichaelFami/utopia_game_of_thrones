255083822dbc049b8d08e7172eda656938671adb
MichaelFami <michaelfami@gmail.com>
2023-09-27 08:45:27 -0500

    reverted to previous version

--- app.js
+++ app.js
@@ -1,322 +1,3 @@
-
-const players = [
-  {
-  name: "Player1",
-  House: "",
-  gold: 200,
-  food: 50,
-  land: 20,
-  houses: 5,
-  farms: 5,
-  banks: 5,
-  army: {
-    generals: 2,
-    soldiers: 300,
-    cavalry: 100,
-    archers: 100,
-    trebuchets: 10,
-    dragons: 0,
-  },
-}, 
-];
-
-let dropDownList = document.querySelector("#numberOfPlayers");
-dropDownList.onchange = (e) => {
-  let selectedI = dropDownList.selectedIndex;
-  let selectedOption = dropDownList.options[selectedI];
-  let selectedNumberOfPlayers = parseInt(selectedOption.value);
-
-  let htmlToAdd=""
-
-
-  for (let i = 1; i < selectedNumberOfPlayers; i++) {
-    players[i] = {
-        name: "Player"+[i+1],
-        House: "",
-        gold: 200,
-        food: 50,
-        land: 20,
-        houses: 5,
-        farms: 5,
-        banks: 5,
-        army: {
-          generals: 2,
-          soldiers: 300,
-          cavalry: 100,
-          archers: 100,
-          trebuchets: 10,
-          dragons: 0,
-        },
-      }
-    }
-    for (let i = 0; i < players.length; i++) {
-     
-      
-    let j=i+1
-    
-      
-    
-htmlToAdd+=`
-<div id="player${[i]}Screen">
-<h3>Player ${[j]}</h3>
-<table class="buildingsTable">
-    <tr><th>Buildings</th></tr>
-    <tr>
-        <th>
-
-        </th>
-        <th>Cost to Build</th>
-        <th>Own</th>
-        
-        <th>Under Construction</th>
-        <th>Build</th>
-    </tr>
-    <tr>
-        <th>Houses</th>
-        <td>5 gold and 1 land</td>
-        <td class="housesBuilt" id="housesBuilt${[i]}">
-            
-        </td>
-        <td class="housesUnderConstruction" id="housesUnderConstruction${[i]}">
-            
-        </td>
-        <td>
-            <input class="buildHouseInput" id="buildHouseInput${[i]}"></input> 
-        </td>
-        
-    </tr>
-    <tr>
-        <th>Farms</th>
-        <td>5 gold and 1 land</td>
-
-        <td id="farmsBuilt${[i]}">
-            
-        </td>
-        <td id="farmsUnderConstruction${[i]}">
-            
-        </td>
-        <td>
-            <input id="buildFarmInput${[i]}"></input> 
-        </td>
-    </tr>
-    <tr>
-        <th>Banks</th>
-        <td>10 gold and 1 land</td>
-
-        <td id="banksBuilt${[i]}">
-            
-        </td>
-        <td id="banksUnderConstruction${[i]}">
-            
-        </td>
-        <td>
-            <input id="buildBankInput${[i]}"></input> 
-        </td>
-    </tr>
-    <tr>
-        <th>Barracks</th>
-        <td>
-            
-        </td>
-    </tr>
-    <tr>
-        <th>Castles</th>
-        <td>
-            
-        </td>
-    </tr>
-    <tr>
-        <th>Armories</th>
-        <td>
-            
-        </td>
-    </tr>
-</table>
-<button id="buildButton${[i]}">Build</button>
-<table id="resourcesTable">
-    <tr><th>Resources</th></tr>
-    <tr>
-        <th>
-
-        </th>
-        <th>Own</th>
-        <th>Per Turn</th>
-       
-    </tr>
-    <tr>
-        <th>Gold</th>
-        <td class="currentGold" id="currentGold${[i]}">
-            
-        </td>
-        <td id="goldPerTurn${[i]}"></td>
-    </tr>
-    <tr>
-        <th>Food</th>
-        <td class="currentFood" id="currentFood${[i]}">
-            
-        </td>
-        <td id="foodPerTurn${[i]}"></td>
-    </tr>
-    <tr>
-        <th>Land</th>
-        <td class="currentLand" id="currentLand${[i]}">
-            
-        </td>
-    </tr>
-    
-</table>
-<table class="militaryTable">
-    <tr><th>Military</th></tr>
-    <tr>
-        <th>
-
-        </th>
-        <th>Own</th>
-        <th>Currently Training</th>
-        <th>Recruit</th>
-    </tr>
-    <tr>
-        <th>Generals</th>
-        <td id="currentGenerals${[i]}">
-            
-        </td>
-        <td></td>
-        <td>
-            <input id="recruitGeneralInput${[i]}"></input> 
-        </td>
-    </tr>
-    <tr>
-        <th>Soldiers</th>
-        <td id="currentSoldiers${[i]}">
-            
-        </td>
-        <td></td>
-        <td>
-            <input id="recruitSoldierInput${[i]}"></input> 
-        </td>
-
-    </tr>
-    <tr>
-        <th>Cavalry</th>
-        <td id="currentCavalry${[i]}">
-            
-        </td>
-        <td></td>
-        <td>
-            <input id="recruitCavalryInput${[i]}"></input> 
-        </td>
-    </tr>
-    <tr>
-        <th>Archers</th>
-        <td id="currentArchers${[i]}">
-            
-        </td>
-        <td></td>
-        <td>
-            <input id="recruitArcherInput${[i]}"></input> 
-        </td>
-    </tr>
-    <tr>
-        <th>Trebuchets</th>
-        <td id="currentTrebuchets${[i]}">
-            
-        </td>
-        <td></td>
-        <td>
-            <input id="recruitTrebuchetInput${[i]}"></input> 
-        </td>
-    </tr>
-    <tr>
-        <th>Dragons</th>
-        <td id="currentDragons${[i]}">
-            
-        </td>
-        <td></td>
-        <td>
-            <input id="recruitDragonInput${[i]}"></input> 
-        </td>
-    </tr>
-    
-</table>
-<button id="recruitButton${[i]}">Recruit</button>
-<table id="opponentsTable">
-    <tr><th>Opponents</th></tr>
-    <tr>
-        <th>
-
-        </th>
-        <th>Military Size</th>
-        <th>Amount of Land</th>
-        <th>Amount of Gold</th>
-        <th>Battle</th>
-    </tr>
-    <tr>
-        <th>Stark</th>
-        <td id="starkArmy">
-            
-        </td>
-        <td>
-            
-        </td>
-        <td>
-            
-        </td>
-        <td>
-            <button class="battleButton" id="stark">Battle!</button> 
-         </td>
-    </tr>
-    <tr>
-        <th>Lannister</th>
-        <td>
-            
-        </td>
-        <td>
-            
-        </td>
-        <td>
-            
-        </td>
-        <td>
-            <button class="battleButton" id="lannister">Battle!</button> 
-         </td>
-    </tr>
-    <tr>
-        <th>Targaryen</th>
-        <td>
-            
-        </td>
-        <td>
-            
-        </td>
-        <td>
-            
-        </td>
-        <td>
-           <button class="battleButton" id="targaryen">Battle!</button> 
-        </td>
-    </tr>
-   
-</table>
-
-
-</div>`
-
-
-
-      
-  }
-
-  let screenContainer = document.querySelector("#screenContainer")
-screenContainer.innerHTML=htmlToAdd
-
-
-
-
-
-
-
-};
-
 let gold;
 let food;
 let land;
@@ -325,7 +6,7 @@ let houses;
 let farms;
 let banks;
 
-let housesBuiltStatsAll = document.querySelectorAll(".housesBuilt");
+const housesBuiltStat = document.querySelector("#housesBuilt");
 const farmsBuiltStat = document.querySelector("#farmsBuilt");
 const banksBuiltStat = document.querySelector("#banksBuilt");
 const buildButton = document.querySelector("#buildButton");
@@ -346,24 +27,61 @@ const currentTrebuchets = document.querySelector("#currentTrebuchets");
 const currentDragons = document.querySelector("#currentDragons");
 
 
+const housesBuiltStatTwo = document.querySelector("#housesBuiltTwo");
+const farmsBuiltStatTwo = document.querySelector("#farmsBuiltTwo");
+const banksBuiltStatTwo = document.querySelector("#banksBuiltTwo");
+const buildButtonTwo = document.querySelector("#buildButtonTwo");
+const battleButtonsTwo = document.querySelectorAll(".battleButtonTwo");
 
+const currentGoldTwo = document.querySelector("#currentGoldTwo");
+const currentFoodTwo = document.querySelector("#currentFoodTwo");
+const currentLandTwo = document.querySelector("#currentLandTwo");
 
-const buildHouses=()=>{
-  for (let i = 0; i < housesBuiltStatsAll.length; i++) {
-    let housesBuiltSpecific = housesBuiltStatsAll[i];
-  let housesBuiltId=document.querySelector(`#${housesBuiltSpecific}`) 
-    housesBuiltId.innerHTML = players[i].houses;
+const goldPerTurnTwo = document.querySelector("#goldPerTurnTwo");
+const foodPerTurnTwo = document.querySelector("#foodPerTurnTwo");
+
+const currentGeneralsTwo = document.querySelector("#currentGeneralsTwo");
+const currentSoldiersTwo = document.querySelector("#currentSoldiersTwo");
+const currentCavalryTwo = document.querySelector("#currentCavalryTwo");
+const currentArchersTwo = document.querySelector("#currentArchersTwo");
+const currentTrebuchetsTwo = document.querySelector("#currentTrebuchetsTwo");
+const currentDragonsTwo = document.querySelector("#currentDragonsTwo");
+
+const INIT_STATE = {
+playerOne:{gold: 200,
+  food: 50,
+  land: 20,
+  houses: 5,
+  farms: 5,
+  banks: 5,
+  army:{
+    generals: 2,
+  soldiers: 300,
+  cavalry: 100,
+  archers: 100,
+  trebuchets: 10,
+  dragons: 1,
+
+  }},
+playerTwo:{gold: 200,
+  food: 50,
+  land: 20,
+  houses: 5,
+  farms: 5,
+  banks: 5,
+  army:{
+    generals: 2,
+  soldiers: 300,
+  cavalry: 100,
+  archers: 100,
+  trebuchets: 10,
+  dragons: 1,
+
+  }}
   
-    console.log("i built a house")
-  }
-
   
-
-}
-
-
-
-
+  
+};
 
 const starkResources = {
   gold: 200,
@@ -392,27 +110,43 @@ const render = () => {
 };
 
 const renderStats = () => {
-  housesBuiltStat.innerHTML = state.houses;
-  farmsBuiltStat.innerHTML = state.farms;
-  banksBuiltStat.innerHTML = state.banks;
-  currentGold.innerHTML = state.gold;
-  currentFood.innerHTML = state.food;
-  currentLand.innerHTML = state.land;
-  goldPerTurn.innerHTML = state.banks * 1;
-  foodPerTurn.innerHTML = state.farms * 1;
-  currentGenerals.innerHTML = state.army.generals;
-  currentSoldiers.innerHTML = state.army.soldiers;
-  currentCavalry.innerHTML = state.army.cavalry;
-  currentArchers.innerHTML = state.army.archers;
-  currentTrebuchets.innerHTML = state.army.trebuchets;
-  currentDragons.innerHTML = state.army.dragons;
+  housesBuiltStat.innerHTML = state.playerOne.houses;
+  farmsBuiltStat.innerHTML = state.playerOne.farms;
+  banksBuiltStat.innerHTML = state.playerOne.banks;
+  currentGold.innerHTML = state.playerOne.gold;
+  currentFood.innerHTML = state.playerOne.food;
+  currentLand.innerHTML = state.playerOne.land;
+  goldPerTurn.innerHTML = state.playerOne.banks * 1;
+  foodPerTurn.innerHTML = state.playerOne.farms * 1;
+  currentGenerals.innerHTML = state.playerOne.army.generals;
+  currentSoldiers.innerHTML = state.playerOne.army.soldiers;
+  currentCavalry.innerHTML = state.playerOne.army.cavalry;
+  currentArchers.innerHTML = state.playerOne.army.archers;
+  currentTrebuchets.innerHTML = state.playerOne.army.trebuchets;
+  currentDragons.innerHTML = state.playerOne.army.dragons;
+
+  housesBuiltStatTwo.innerHTML = state.playerTwo.houses;
+  farmsBuiltStatTwo.innerHTML = state.playerTwo.farms;
+  banksBuiltStatTwo.innerHTML = state.playerTwo.banks;
+  currentGoldTwo.innerHTML = state.playerTwo.gold;
+  currentFoodTwo.innerHTML = state.playerTwo.food;
+  currentLandTwo.innerHTML = state.playerTwo.land;
+  goldPerTurnTwo.innerHTML = state.playerTwo.banks * 1;
+  foodPerTurnTwo.innerHTML = state.playerTwo.farms * 1;
+  currentGeneralsTwo.innerHTML = state.playerTwo.army.generals;
+  currentSoldiersTwo.innerHTML = state.playerTwo.army.soldiers;
+  currentCavalryTwo.innerHTML = state.playerTwo.army.cavalry;
+  currentArchersTwo.innerHTML = state.playerTwo.army.archers;
+  currentTrebuchetsTwo.innerHTML = state.playerTwo.army.trebuchets;
+  currentDragonsTwo.innerHTML = state.playerTwo.army.dragons;
+
+
 };
 
 const init = () => {
-  // state = {
-  //   ...players[0],
-  // };
-  
+  state = {
+    ...INIT_STATE,
+  };
 
   cycles = 0;
 
@@ -421,8 +155,11 @@ const init = () => {
   render();
 };
 const generateResources = () => {
-  state.gold = state.gold + state.banks * 1;
-  state.food = state.food + state.farms * 1;
+  state.playerOne.gold = state.playerOne.gold + state.playerOne.banks * 1;
+  state.playerOne.food = state.playerOne.food + state.playerOne.farms * 1;
+
+  state.playerTwo.gold = state.playerTwo.gold + state.playerTwo.banks * 1;
+  state.playerTwo.food = state.playerTwo.food + state.playerTwo.farms * 1;
 };
 
 const runGame = () => {
@@ -430,9 +167,8 @@ const runGame = () => {
   if (cycles < 1000) {
     // updateStats()
 
-    // generateResources();
+    generateResources();
     console.log("i updated");
-    console.log(housesBuiltStatsAll)
   } else {
     return gameOver();
   }
@@ -451,17 +187,19 @@ const handleBtnClick = () => {
   let bankInput = parseInt(document.querySelector("#buildBankInput").value);
   let totalBeingBuilt = houseInput + farmInput + bankInput;
   let totalCost = houseInput * 5 + farmInput * 5 + bankInput * 10;
+  console.log(houseInput)
+  console.log(farmInput)
   console.log(totalBeingBuilt);
   console.log(totalCost);
-  if (totalCost > state.gold) {
+  if (totalCost > state.playerOne.gold) {
     alert("You don't have enough gold");
-  } else if (totalBeingBuilt > state.land) {
+  } else if (totalBeingBuilt > state.playerOne.land) {
     alert("You don't have enough land to build on");
   } else {
     if (houseInput != 0) {
-      state.gold = state.gold - houseInput * 5;
-      state.land = state.land - houseInput;
-      state.houses = state.houses + parseInt(houseInput);
+      state.playerOne.gold = state.playerOne.gold - houseInput * 5;
+      state.playerOne.land = state.playerOne.land - houseInput;
+      state.playerOne.houses = state.playerOne.houses + parseInt(houseInput);
 
       console.log("houseadded");
       render();
@@ -470,10 +208,10 @@ const handleBtnClick = () => {
     }
 
     if (farmInput != 0) {
-      state.gold = state.gold - farmInput * 5;
-      state.land = state.land - farmInput;
+      state.playerOne.gold = state.playerOne.gold - farmInput * 5;
+      state.playerOne.land = state.playerOne.land - farmInput;
 
-      state.farms = state.farms + parseInt(farmInput);
+      state.playerOne.farms = state.playerOne.farms + parseInt(farmInput);
 
       console.log("farmadded");
       render();
@@ -482,10 +220,10 @@ const handleBtnClick = () => {
     }
 
     if (bankInput != 0) {
-      state.gold = state.gold - bankInput * 10;
-      state.land = state.land - bankInput;
+      state.playerOne.gold = state.playerOne.gold - bankInput * 10;
+      state.playerOne.land = state.playerOne.land - bankInput;
 
-      state.banks = state.banks + parseInt(bankInput);
+      state.playerOne.banks = state.playerOne.banks + parseInt(bankInput);
 
       console.log("bankadded");
       render();
@@ -497,46 +235,92 @@ const handleBtnClick = () => {
   render();
 };
 
-const battleCalculator = (chosenOpponentArmy) => {
-  let myMilitaryPower =
-    state.army.generals * 50 +
-    state.army.soldiers * 1 +
-    state.army.cavalry * 3 +
-    state.army.archers * 2 +
-    state.army.trebuchets * 100 +
-    state.army.dragons * 500;
-  let opponentMilitaryPower =
-    chosenOpponentArmy.generals * 50 +
-    chosenOpponentArmy.soldiers * 1 +
-    chosenOpponentArmy.cavalry * 3 +
-    chosenOpponentArmy.archers * 2 +
-    chosenOpponentArmy.trebuchets * 100 +
-    chosenOpponentArmy.dragons * 500;
-  if (myMilitaryPower > opponentMilitaryPower) {
-    Object.keys(chosenOpponentArmy).forEach((element) => {
-      chosenOpponentArmy[element] = Math.floor(
-        chosenOpponentArmy[element] * 0.5
-      );
-      console.log(chosenOpponentArmy);
-    });
-  } else if (opponentMilitaryPower > myMilitaryPower) {
-    state.army.forEach((element) => {
-      state.army[element] = Math.floor(state.army[element] * 0.5);
-    });
+const handleBtnClickTwo = () => {
+  let houseInput = parseInt(document.querySelector("#buildHouseInputTwo").value);
+  let farmInput = parseInt(document.querySelector("#buildFarmInputTwo").value);
+  let bankInput = parseInt(document.querySelector("#buildBankInputTwo").value);
+  let totalBeingBuilt = houseInput + farmInput + bankInput;
+  let totalCost = houseInput * 5 + farmInput * 5 + bankInput * 10;
+  console.log(totalBeingBuilt);
+  console.log(totalCost);
+  if (totalCost > state.playerTwo.gold) {
+    alert("You don't have enough gold");
+  } else if (totalBeingBuilt > state.land) {
+    alert("You don't have enough land to build on");
   } else {
-    return;
+    if (houseInput != 0) {
+      state.playerTwo.gold = state.playerTwo.gold - houseInput * 5;
+      state.playerTwo.land = state.playerTwo.land - houseInput;
+      state.playerTwo.houses = state.playerTwo.houses + parseInt(houseInput);
+
+      console.log("houseadded");
+      render();
+    } else {
+      return;
+    }
+
+    if (farmInput != 0) {
+      state.playerTwo.gold = state.playerTwo.gold - farmInput * 5;
+      state.playerTwo.land = state.playerTwo.land - farmInput;
+
+      state.playerTwo.farms = state.playerTwo.farms + parseInt(farmInput);
+
+      console.log("farmadded");
+      render();
+    } else {
+      return;
+    }
+
+    if (bankInput != 0) {
+      state.playerTwo.gold = state.playerTwo.gold - bankInput * 10;
+      state.playerTwo.land = state.playerTwo.land - bankInput;
+
+      state.playerTwo.banks = state.playerTwo.banks + parseInt(bankInput);
+
+      console.log("bankadded");
+      render();
+    } else {
+      return;
+    }
   }
+
+  render();
 };
+
+const battleCalculator=(chosenOpponentArmy)=>{
+    let myMilitaryPower = (
+        (state.army.generals*50)+(state.army.soldiers*1)+(state.army.cavalry*3)+(state.army.archers*2)+(state.army.trebuchets*100)+(state.army.dragons*500)
+    )
+    let opponentMilitaryPower=(
+        (chosenOpponentArmy.generals*50)+(chosenOpponentArmy.soldiers*1)+(chosenOpponentArmy.cavalry*3)+(chosenOpponentArmy.archers*2)+(chosenOpponentArmy.trebuchets*100)+(chosenOpponentArmy.dragons*500)
+    )
+    if (myMilitaryPower>opponentMilitaryPower) {
+        Object.keys(chosenOpponentArmy).forEach(element => {
+            chosenOpponentArmy[element] =Math.floor(chosenOpponentArmy[element]*0.5)
+            console.log(chosenOpponentArmy)
+
+        });
+    } else if(opponentMilitaryPower>myMilitaryPower){
+        state.army.forEach(element => {
+            state.army[element] =Math.floor(state.army[element]*0.5)
+        });
+    }else{
+        return
+    }
+   
+}
 
 battleButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    let chosenOpponentArmy = opponents[button.id].army;
-    battleCalculator(chosenOpponentArmy);
-    render();
+    let chosenOpponentArmy = opponents[button.id].army
+    battleCalculator(chosenOpponentArmy)
+    render()
   });
+  
 });
 
 buildButton.addEventListener("click", handleBtnClick);
+buildButtonTwo.addEventListener("click", handleBtnClickTwo);
 
 init();
 
